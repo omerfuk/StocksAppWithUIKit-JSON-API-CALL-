87f312781804bfd6f6c3ab08ccdd5be2c9f93544
Ömer Faruk Kılıçaslan <omerrr_2001@hotmail.com>
2022-06-02 16:03:38 +0300

    Initial Commit

---
+++ StocksAppWithUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StocksAppWithUIKit
+//
+//  Created by Ömer Faruk Kılıçaslan on 2.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
